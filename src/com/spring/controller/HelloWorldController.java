da61d0427a29f1da737a7dae286a995a18edf474
Rajesh <Rajesh@Rajesh-PC>
2016-11-26 10:50:33 +0530

    first commit

---
+++ src/com/spring/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.spring.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+public class HelloWorldController {
+
+}
